b4ef24ca39b98b3adf746444c165b39278a37bdb
Feetvi <razer-key@mail.ru>
2018-02-22 21:18:07 +0300

    my solution of task

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   	for (var i = 0; i < preferences.length; i++) {
   		var	f = preferences[i];  		
  		var s = preferences[f-1]; 		 
- 		var t = preferences[s-1];
- 		var fi = i;
- 		var si = f - 1;
+ 		var t = preferences[s-1]; 		
+ 		var si = f - 1; // индексы второго и третьего элементов (индексом первого элемента считаем i)
  		var ti = s - 1; 		
  		if (f != s) { // проверка на одинаковые значения и индексы
- 			if (t-1 === fi && f-1 === si && s-1 === ti) { // проверка на "любовный треугольник"				
+ 			if (t-1 === i && f-1 === si && s-1 === ti) { // проверка на "любовный треугольник"				
  				res++;
  				preferences[i] = 0; // очищаем значение, чтобы избежать повторяющихся пар	
  			} 
